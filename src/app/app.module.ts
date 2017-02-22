8735b129589df6e3bc857003a0eddfae8a549bbb
Jesse Baril <jessebaril.programmer@gmail.com>
2017-02-22 00:24:15 -0500

    Created base Structure of N64 Games List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,37 @@
+/*  Created: February 21, 2017
+    N64 Game Collection List
+    Jesse Baril
+    200226521
+    This is the app module page for the to-do list */
+
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBJvuq8yPHCL1F5hZw2yFL-OIKms1Dhcts",
+    authDomain: "myionicproject-f5d7b.firebaseapp.com",
+    databaseURL: "https://myionicproject-f5d7b.firebaseio.com",
+    storageBucket: "myionicproject-f5d7b.appspot.com",
+    messagingSenderId: "689177205561"
+  };
+  
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    HomePage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,108 @@
+/*  Created: February 21, 2017
+    N64 Game Collection List
+    Jesse Baril
+    200226521
+    This is the logic for the Game Collection list app */
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, ActionSheetController } from 'ionic-angular';
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  listGames: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, 
+  af: AngularFire, public actionSheetCtrl: ActionSheetController) {
+    this.listGames = af.database.list('/todoList');
   }
 
+  /*addItem
+    This method activates a popup for the user 
+    to add a new game to the list */
+  addItem(){    
+    let prompt = this.alertCtrl.create({
+    title: 'N64 Game',
+    message: "Add a new N64 Game to the list.",
+    inputs: [
+      {
+        name: 'game',
+        placeholder: 'Value'
+      },
+    ],
+    buttons: [
+       {
+         text: 'Cancel',
+         handler: data => {
+           console.log('Cancel clicked');
+         }
+       },
+       {
+         text: 'Add',
+         handler: data => {
+           this.listGames.push({
+             item: data.item,
+             done: false
+           });
+         }
+       }
+     ]
+   });
+   prompt.present();
+   }
+  /*removeGame
+    This method removes the selected game from the games list */
+  removeGame(itemId: string){
+    this.listGames.remove(itemId);
+  }
+  /*updateItem
+    This method activates a popup for the user
+    to edit the details of a game */
+  updateItem(itemId, itemDescription){
+    let prompt = this.alertCtrl.create({
+      title: 'Estimated Value',
+      message: "Update the estimated value for this N64 Game.",
+      inputs: [
+        {
+          name: 'game',
+          placeholder: 'Description',
+          value: itemDescription
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Update',
+          handler: data => {
+            this.listGames.update(itemId, {
+              item: data.item
+            });
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+  /*This method switches a game from owned 
+    to wanted or vice versa.*/
+  switchComplete(itemId, itemCompletion){
+    if(itemCompletion == true){
+      this.listGames.update(itemId, {
+        done: false
+      });
+    }
+    if(itemCompletion == false){
+      this.listGames.update(itemId, {
+        done: true
+      });
+    }
+  }
 }
